570f4ced911a33727d7df3ffeea305c009191fac
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-03-04 17:15:57 +0100

    Set root route to api/batches#new

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ SpreadsheetUpload::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  root 'batches#new'
+  root 'api/batches#new'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
