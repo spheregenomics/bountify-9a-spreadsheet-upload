a7212fc7c8e183dffa88f182d5708357a27e1951
Jakob Krigovsky <jakob.krigovsky@gmail.com>
2014-03-04 17:59:29 +0100

    Uncomment attr_accessible from BatchDetail’s model
    This is necessary so that seeds.rb can insert its data.

--- app/models/batch_detail.rb
+++ app/models/batch_detail.rb
@@ -4,9 +4,9 @@ class BatchDetail < ActiveRecord::Base
  
   
 
-  #attr_accessible :primer3_parameters_attributes, :step, :status, :gene, :grch37_start, :grch37_stop, :status, :batch_id, :cosmic_mut_id, 
-  #                :bin, :chrom, :chrom_start, :chrom_end, :primer3_raw, :primer3_formatted, 
-  #                :forward_base_pair_offset, :reverse_base_pair_offset
+  attr_accessible :primer3_parameters_attributes, :step, :status, :gene, :grch37_start, :grch37_stop, :status, :batch_id, :cosmic_mut_id, 
+                 :bin, :chrom, :chrom_start, :chrom_end, :primer3_raw, :primer3_formatted, 
+                 :forward_base_pair_offset, :reverse_base_pair_offset
 
   STATUS_STEP = %w(select prepare locate sequence assay)
   STATUS_OPTIONS = %w(initialize processing completed error)
